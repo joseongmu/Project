4be72e4d6587f3aea7c66b9ef8d3c3512bfdcb06
joseongmu <rpspdlxm@naver.com>
2012-11-25 19:06:34 +0900

    modify Project

--- WebContent1/Menu/DashBoard/Organizing/upload.jsp
+++
@@ -1,5 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-			<script>
-								location.replace("http://localhost:8080/gowith/Menu/DashBoard/Organizing/Create_Organizing.jsp?next=calender");
-			</script>
--- WebContent1/js/index.js
+++
@@ -1,9 +0,0 @@
-
-			  (function(window) {
-			   			var gidsy = window.gidsy = window.gidsy || {};
-			
-				    	gidsy._dom_ready_queue = [];
-			  	  	gidsy.on_dom_ready = function(func) {
-			  	    gidsy._dom_ready_queue.push(func);
-			  	  };
-			  	})(this);
